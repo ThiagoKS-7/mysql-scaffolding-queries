f3259b912ebd8dc664ff9648b758fbb8cc85fa8c
Thiago Kasper de Souza <83460816+ThiagoKS-7@users.noreply.github.com>
2023-07-12 01:05:12 -0300

    Create insere_tubo.sql

---
+++ insere_tubo.sql
@@ -0,0 +1,15 @@
+insert into
+    tubo (
+        id,
+        diametro,
+        comprimentoEmM,
+        espessuraEmMM,
+        material
+    )
+VALUES (
+        'D50x3xSAE1020',
+        50,
+        3,
+        3,
+        'aco_1020'
+    );
